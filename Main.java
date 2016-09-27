25f70440fdc1e5fb7b5c50974365494aae1f2d0a
andrewcchau <andrewcchau@utexas.edu>
2016-09-26 22:02:14 -0500

    Swap lines
    Swapped sysout with printLadder in main()

--- Main.java
+++ Main.java
@@ -50,8 +50,8 @@ public class Main {
 		//if /quit or isn't 2 words, continue. Otherwise quit
 		while(!words.isEmpty() && !words.get(0).equals("QUIT") && words.size() == 2){
 
-			ArrayList<String> end2 = getWordLadderBFS(words.get(0),words.get(1));
-			System.out.println(end2);
+			ArrayList<String> end = getWordLadderBFS(words.get(0),words.get(1));
+			printLadder(end);
 
 			//since we edit the dictionary in some of the methods
 			initialize();
